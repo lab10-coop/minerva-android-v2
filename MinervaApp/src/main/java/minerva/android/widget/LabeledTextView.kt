977c78f33064330a3932cd21259086efb2171cbd
Witold Olejniczak <w.olejniczak@binarapps.com>
2020-07-31 12:13:22 +0000

    Crate show identity screen

---
+++ MinervaApp/src/main/java/minerva/android/widget/LabeledTextView.kt
@@ -0,0 +1,28 @@
+package minerva.android.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.labeled_text_view.view.*
+import minerva.android.R
+import minerva.android.extension.gone
+import minerva.android.extension.visible
+import minerva.android.extension.visibleOrInvisible
+
+class LabeledTextView(context: Context, attrs: AttributeSet? = null) : FrameLayout(context, attrs) {
+
+    fun setTitleAndBody(titleText: String, bodyText: String) {
+        visible()
+        title.apply {
+            visibleOrInvisible(titleText.isNotBlank())
+            text = titleText
+        }
+        body.text = bodyText
+    }
+
+    init {
+        inflate(context, R.layout.labeled_text_view, this)
+        gone()
+    }
+}
