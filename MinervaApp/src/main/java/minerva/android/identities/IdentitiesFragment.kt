00acdde37b7c7e694a830aadcb956f22eca8e6dd
Jakub Witczak <j.witczak@binarapps.com>
2020-08-18 13:22:04 +0000

    Split identities screen

--- MinervaApp/src/main/java/minerva/android/identities/IdentitiesFragment.kt
+++ MinervaApp/src/main/java/minerva/android/identities/IdentitiesFragment.kt
@@ -4,89 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import kotlinx.android.synthetic.main.recycler_view_layout.*
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.identities_fragment_layout.*
 import minerva.android.R
-import minerva.android.identities.adapter.IdentityAdapter
-import minerva.android.identities.adapter.IdentityFragmentListener
-import minerva.android.kotlinUtils.event.EventObserver
-import minerva.android.walletmanager.model.Credential
-import minerva.android.walletmanager.model.Identity
-import minerva.android.walletmanager.model.MinervaPrimitive
-import minerva.android.walletmanager.model.Service
-import minerva.android.wrapped.startEditIdentityWrappedActivity
-import minerva.android.wrapped.startIdentityAddressWrappedActivity
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import minerva.android.identities.adapter.IdentitiesPagerAdapter
 
-class IdentitiesFragment : Fragment(), IdentityFragmentListener {
-
-    private val viewModel: IdentitiesViewModel by viewModel()
-    private val identityAdapter = IdentityAdapter(this)
+class IdentitiesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.recycler_view_layout, container, false)
+        inflater.inflate(R.layout.identities_fragment_layout, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupRecycleView()
-        setupLiveData()
+        identityViewPager.adapter = IdentitiesPagerAdapter(this)
+        TabLayoutMediator(identityTabs, identityViewPager) { tab, position ->
+            when (position) {
+                MY_IDENTITIES_POSITION -> tab.text = getString(R.string.my_identities_label)
+                CONTACTS_POSITION -> tab.text = getString(R.string.contacts_label)
+                CREDENTIALS_POSITION -> tab.text = getString(R.string.credentials_label)
+            }
+        }.attach()
     }
 
-    private fun setupRecycleView() {
-        recyclerView.apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = identityAdapter
-        }
-    }
-
-    private fun showError(error: Throwable) {
-        Toast.makeText(this.context, error.message, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun setupLiveData() {
-        viewModel.apply {
-            walletConfigLiveData.observe(viewLifecycleOwner, Observer { identityAdapter.updateList(it.identities.toMutableList()) })
-            identityRemovedLiveData.observe(viewLifecycleOwner, EventObserver { activity?.invalidateOptionsMenu() })
-            errorLiveData.observe(viewLifecycleOwner, EventObserver { showError(it) })
-        }
-    }
-
-    override fun showIdentity(identity: Identity, position: Int) {
-        startIdentityAddressWrappedActivity(requireContext(), identity.name, position)
-    }
-
-    override fun onIdentityRemoved(identity: Identity) {
-        showRemoveDialog(identity.name, R.string.remove_identity_dialog_message) { viewModel.removeIdentity(identity) }
-    }
-
-    override fun onIdentityEdit(position: Int, name: String) {
-        startEditIdentityWrappedActivity(requireContext(), position, name)
-    }
-
-    override fun onBindedItemDeleted(minervaPrimitive: MinervaPrimitive) {
-        when (minervaPrimitive) {
-            is Service -> TODO("Handle deleting binded service")
-            is Credential -> showRemoveDialog(getString(R.string.remove_credential_dialog_title), R.string.remove_credential_dialog_message)
-            { viewModel.removeCredential(minervaPrimitive) }
-        }
-    }
-
-    private fun showRemoveDialog(title: String, messageId: Int, removeAction: () -> Unit) {
-        context?.let { context ->
-            MaterialAlertDialogBuilder(context, R.style.AlertDialogMaterialTheme)
-                .setBackground(context.getDrawable(R.drawable.rounded_white_background))
-                .setTitle(title)
-                .setMessage(getString(messageId))
-                .setPositiveButton(R.string.remove) { dialog, _ ->
-                    removeAction()
-                    dialog.dismiss()
-                }
-                .setNegativeButton(R.string.cancel) { dialog, _ -> dialog.dismiss() }
-                .show()
-        }
+    companion object {
+        const val MY_IDENTITIES_POSITION = 0
+        const val CONTACTS_POSITION = 1
+        const val CREDENTIALS_POSITION = 2
     }
 }
---
+++ MinervaApp/src/main/java/minerva/android/identities/myIdentities/MyIdentitiesFragment.kt
@@ -0,0 +1,97 @@
+package minerva.android.identities.myIdentities
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.recycler_view_layout.*
+import minerva.android.R
+import minerva.android.identities.adapter.IdentityAdapter
+import minerva.android.identities.adapter.IdentityFragmentListener
+import minerva.android.kotlinUtils.event.EventObserver
+import minerva.android.walletmanager.model.Credential
+import minerva.android.walletmanager.model.Identity
+import minerva.android.walletmanager.model.MinervaPrimitive
+import minerva.android.walletmanager.model.Service
+import minerva.android.wrapped.startEditIdentityWrappedActivity
+import minerva.android.wrapped.startIdentityAddressWrappedActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MyIdentitiesFragment : Fragment(), IdentityFragmentListener {
+
+    private val viewModel: MyIdentitiesViewModel by viewModel()
+    private val identityAdapter = IdentityAdapter(this)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.recycler_view_layout, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecycleView()
+        setupLiveData()
+    }
+
+    private fun setupRecycleView() {
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = identityAdapter
+        }
+    }
+
+    private fun showError(error: Throwable) {
+        Toast.makeText(this.context, error.message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setupLiveData() {
+        viewModel.apply {
+            walletConfigLiveData.observe(viewLifecycleOwner, Observer { identityAdapter.updateList(it.identities.toMutableList()) })
+            identityRemovedLiveData.observe(viewLifecycleOwner, EventObserver { activity?.invalidateOptionsMenu() })
+            errorLiveData.observe(viewLifecycleOwner, EventObserver { showError(it) })
+        }
+    }
+
+    override fun showIdentity(identity: Identity, position: Int) {
+        startIdentityAddressWrappedActivity(requireContext(), identity.name, position)
+    }
+
+    override fun onIdentityRemoved(identity: Identity) {
+        showRemoveDialog(identity.name, R.string.remove_identity_dialog_message) { viewModel.removeIdentity(identity) }
+    }
+
+    override fun onIdentityEdit(position: Int, name: String) {
+        startEditIdentityWrappedActivity(requireContext(), position, name)
+    }
+
+    override fun onBindedItemDeleted(minervaPrimitive: MinervaPrimitive) {
+        when (minervaPrimitive) {
+            is Service -> TODO("Handle deleting binded service")
+            is Credential -> showRemoveDialog(getString(R.string.remove_credential_dialog_title), R.string.remove_credential_dialog_message)
+            { viewModel.removeCredential(minervaPrimitive) }
+        }
+    }
+
+    private fun showRemoveDialog(title: String, messageId: Int, removeAction: () -> Unit) {
+        context?.let { context ->
+            MaterialAlertDialogBuilder(context, R.style.AlertDialogMaterialTheme)
+                .setBackground(context.getDrawable(R.drawable.rounded_white_background))
+                .setTitle(title)
+                .setMessage(getString(messageId))
+                .setPositiveButton(R.string.remove) { dialog, _ ->
+                    removeAction()
+                    dialog.dismiss()
+                }
+                .setNegativeButton(R.string.cancel) { dialog, _ -> dialog.dismiss() }
+                .show()
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MyIdentitiesFragment()
+    }
+}
