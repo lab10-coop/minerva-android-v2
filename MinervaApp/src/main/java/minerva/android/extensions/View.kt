d604afbbf77bd52205e3f14cb4f0d3c136582076
Jakub Witczak <j.witczak@binarapps.com>
2020-09-07 14:11:15 +0000

    Add default credential icon

--- MinervaApp/src/main/java/minerva/android/extensions/View.kt
+++ MinervaApp/src/main/java/minerva/android/extensions/View.kt
@@ -8,8 +8,8 @@ import minerva.android.R
 
 fun ImageView.loadImageUrl(url: String?) {
     if (url.isNullOrEmpty()) {
-        Glide.with(context).load(R.mipmap.ic_minerva).into(this)
+        Glide.with(context).load(R.drawable.ic_default_credential).into(this)
         return
     }
-    GlideToVectorYou.init().with(context).setPlaceHolder(-1, R.drawable.ic_minerva_icon).load(Uri.parse(url), this)
+    GlideToVectorYou.init().with(context).setPlaceHolder(-1, R.drawable.ic_default_credential).load(Uri.parse(url), this)
 }
