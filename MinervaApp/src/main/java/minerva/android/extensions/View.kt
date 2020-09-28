7e5298e56abd86d96e7ad3909db224c061d831e1
Witold Olejniczak <w.olejniczak@binarapps.com>
2020-09-28 11:07:15 +0000

    Default icon on server response error

--- MinervaApp/src/main/java/minerva/android/extensions/View.kt
+++ MinervaApp/src/main/java/minerva/android/extensions/View.kt
@@ -1,9 +1,12 @@
 package minerva.android.extensions
 
 import android.net.Uri
+import android.os.Handler
+import android.os.Looper
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
+import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYouListener
 import minerva.android.R
 
 fun ImageView.loadImageUrl(url: String?) {
@@ -11,5 +14,15 @@ fun ImageView.loadImageUrl(url: String?) {
         Glide.with(context).load(R.drawable.ic_default_credential).into(this)
         return
     }
-    GlideToVectorYou.init().with(context).setPlaceHolder(-1, R.drawable.ic_default_credential).load(Uri.parse(url), this)
+    GlideToVectorYou.init().with(context)
+        .withListener(
+            object : GlideToVectorYouListener {
+                override fun onLoadFailed() {
+                    Handler(Looper.getMainLooper()).post {
+                        Glide.with(context).load(R.drawable.ic_default_credential).into(this@loadImageUrl)
+                    }
+                }
+                override fun onResourceReady() { }
+            })
+        .setPlaceHolder(R.drawable.ic_default_credential, R.drawable.ic_default_credential).load(Uri.parse(url), this)
 }
