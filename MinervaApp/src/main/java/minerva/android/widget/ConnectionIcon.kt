9df185acf7aa7360b189081bffe671b6433f7638
kubel <j.witczak@binarapps.com>
2021-01-11 09:23:24 +0100

    change to frame layout

--- MinervaApp/src/main/java/minerva/android/widget/ConnectionIcon.kt
+++ MinervaApp/src/main/java/minerva/android/widget/ConnectionIcon.kt
@@ -3,6 +3,7 @@ package minerva.android.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.FrameLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
 import minerva.android.databinding.ConnectionIconBinding
@@ -11,9 +12,9 @@ class ConnectionIcon @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ConstraintLayout(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr) {
 
-    internal val binding: ConnectionIconBinding =
+    private val binding: ConnectionIconBinding =
         ConnectionIconBinding.inflate(LayoutInflater.from(context), this, true)
 
     fun setIcon(resId: Int) {
