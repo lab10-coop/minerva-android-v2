b7367e30d090aa75962c830e68011a1118fc1a63
kubel <j.witczak@binarapps.com>
2021-02-11 09:27:17 +0100

    bug fix and clean up crypto repo

--- CryptographyProvider/src/main/java/minerva/android/cryptographyProvider/repository/CryptographyRepository.kt
+++ CryptographyProvider/src/main/java/minerva/android/cryptographyProvider/repository/CryptographyRepository.kt
@@ -4,7 +4,7 @@ import io.reactivex.Single
 import minerva.android.cryptographyProvider.repository.model.DerivedKeys
 
 interface CryptographyRepository {
-    fun createMasterSeed(derivationPath: String): Single<Triple<String, String, String>> //seed, publicKey, privateKey
+    fun createMasterSeed(): Single<Triple<String, String, String>> //seed, publicKey, privateKey
     fun getMnemonicForMasterSeed(seed: String): String
     fun calculateDerivedKeys(
         seed: String,
@@ -13,11 +13,7 @@ interface CryptographyRepository {
         isTestNet: Boolean = true
     ): Single<DerivedKeys>
 
-    fun restoreMasterSeed(
-        mnemonic: String,
-        derivationPath: String
-    ): Single<Triple<String, String, String>> //seed, publicKey, privateKey
-
+    fun restoreMasterSeed(mnemonic: String): Single<Triple<String, String, String>> //seed, publicKey, privateKey
     fun validateMnemonic(mnemonic: String): List<String>
     fun decodeJwtToken(jwtToken: String): Single<Map<String, Any?>>
     fun createJwtToken(payload: Map<String, Any?>, privateKey: String): Single<String>
