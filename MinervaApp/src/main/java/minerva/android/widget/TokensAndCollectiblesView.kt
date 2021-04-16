81e0c8b73a50ccf835f90ddd63d61665524832ce
w.olejniczak <w.olejniczak@binarapps.com>
2021-04-16 06:50:37 +0200

    Some refactors before review

--- MinervaApp/src/main/java/minerva/android/widget/TokensAndCollectiblesView.kt
+++ MinervaApp/src/main/java/minerva/android/widget/TokensAndCollectiblesView.kt
@@ -4,7 +4,6 @@ import android.animation.ObjectAnimator
 import android.content.Context
 import android.transition.TransitionManager
 import android.util.AttributeSet
-import android.util.Log
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
