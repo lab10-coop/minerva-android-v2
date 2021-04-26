3b99658503d3df22bd7e8170fe438faa7d409db1
w.olejniczak <w.olejniczak@binarapps.com>
2021-04-26 18:13:45 +0200

    Some additional refactors

--- WalletManager/src/main/java/minerva/android/walletmanager/utils/TokenUtils.kt
+++ WalletManager/src/main/java/minerva/android/walletmanager/utils/TokenUtils.kt
@@ -2,7 +2,6 @@ package minerva.android.walletmanager.utils
 
 import java.util.*
 
-//TODO klop add tests
 object TokenUtils {
     fun generateTokenHash(chainId: Int, address: String) = "$chainId$address".toLowerCase(Locale.ROOT)
 }
