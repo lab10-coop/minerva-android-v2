ba5986fd5ccb2123d2cbbfeb879ec73c4c25b79d
w.olejniczak <w.olejniczak@binarapps.com>
2021-05-19 14:41:03 +0200

    Some refactors before review

--- MinervaApp/src/main/java/minerva/android/widget/state/AppUIStateImpl.kt
+++ MinervaApp/src/main/java/minerva/android/widget/state/AppUIStateImpl.kt
@@ -3,8 +3,7 @@ package minerva.android.widget.state
 class AppUIStateImpl : AppUIState {
     private var accountStates = mutableMapOf<Int, AccountWidgetState>()
 
-    override fun getAccountWidgetState(index: Int): AccountWidgetState =
-        (accountStates[index] ?: AccountWidgetState())
+    override fun getAccountWidgetState(index: Int): AccountWidgetState = (accountStates[index] ?: AccountWidgetState())
 
     override fun updateAccountWidgetState(index: Int, state: AccountWidgetState) {
         accountStates[index] = state
