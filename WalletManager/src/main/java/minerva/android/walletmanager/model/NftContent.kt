ea28cdfa4820707514f8a8ae4543d904ee0ef743
Szymon Rzad <szymon.rzad@binarapps.com>
2022-02-11 14:10:04 +0100

    TokenUri and other nft details for NFTs are now fetched from TokenOwnedApi

--- WalletManager/src/main/java/minerva/android/walletmanager/model/NftContent.kt
+++ WalletManager/src/main/java/minerva/android/walletmanager/model/NftContent.kt
@@ -3,7 +3,8 @@ package minerva.android.walletmanager.model
 import minerva.android.kotlinUtils.Empty
 
 data class NftContent(
-    val imageUri: String = String.Empty,
-    val contentType: ContentType = ContentType.IMAGE,
-    val animationUri: String = String.Empty
+    var imageUri: String = String.Empty,
+    var contentType: ContentType = ContentType.INVALID,
+    var animationUri: String = String.Empty,
+    var tokenUri: String = String.Empty
 )
--- WalletManager/src/main/java/minerva/android/walletmanager/model/mappers/TokensOwnedToERCToken.kt
+++ WalletManager/src/main/java/minerva/android/walletmanager/model/mappers/TokensOwnedToERCToken.kt
@@ -2,9 +2,12 @@ package minerva.android.walletmanager.model.mappers
 
 import minerva.android.apiProvider.model.TokensOwnedPayload
 import minerva.android.kotlinUtils.Empty
+import minerva.android.walletmanager.model.ContentType
+import minerva.android.walletmanager.model.NftContent
 import minerva.android.walletmanager.model.token.ERCToken
 import minerva.android.walletmanager.model.token.TokenType
 import minerva.android.walletmanager.model.token.Tokens
+import minerva.android.walletmanager.utils.parseIPFSContentUrl
 
 object TokensOwnedToERCToken {
 
@@ -20,7 +23,12 @@ object TokensOwnedToERCToken {
             type = tokenType,
             collectionName = if (tokenType.isNft()) token.name else null,
             tokenId = if (tokenType.isNft()) token.id else null
-        )
+        ).apply {
+            token.tokenJson?.let {
+                handleNftContent(it)
+            }
+            nftContent.tokenUri = token.tokenURI
+        }
     }
 
     private fun mapToTokenType(types: List<String>): TokenType = when {
@@ -28,5 +36,20 @@ object TokensOwnedToERCToken {
         types.contains(Tokens.ERC_721.type) -> TokenType.ERC721
         types.contains(Tokens.ERC_20.type) -> TokenType.ERC20
         else -> TokenType.INVALID
+    }
+
+
+
+    private fun ERCToken.handleNftContent(tokenJson: TokensOwnedPayload.TokenOwned.TokenJson)  {
+        val type = when {
+            !tokenJson.animationUri.isNullOrBlank() -> ContentType.VIDEO
+            else -> ContentType.IMAGE
+        }
+        nftContent = NftContent(
+            parseIPFSContentUrl(tokenJson.image ?: String.Empty),
+            type,
+            parseIPFSContentUrl(tokenJson.animationUri ?: String.Empty))
+        tokenJson.description?.let{description = it}
+        tokenJson.name?.let{name = it}
     }
 }
