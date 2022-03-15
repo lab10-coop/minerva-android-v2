1d420cc7c984223879b5804433d58f82132350b1
Peter Grassberger <peter.grassberger@lab10.coop>
2022-03-15 16:56:07 +0100

    Supertoken via tokensownedapi

--- WalletManager/src/main/java/minerva/android/walletmanager/model/token/Tokens.kt
+++ WalletManager/src/main/java/minerva/android/walletmanager/model/token/Tokens.kt
@@ -3,5 +3,6 @@ package minerva.android.walletmanager.model.token
 enum class Tokens(val type: String) {
     ERC_20("ERC-20"),
     ERC_721("ERC-721"),
-    ERC_1155("ERC-1155")
+    ERC_1155("ERC-1155"),
+    SUPER_TOKEN("SuperToken")
 }
