06a585ca360812e15fb103ff6403aef95a151cdb
PeterTheOne <petertheone@gmail.com>
2022-09-14 16:09:09 +0200

    logToFirebase: only include public key if initialized

--- WalletManager/src/main/java/minerva/android/walletmanager/utils/logger/LoggerImpl.kt
+++ WalletManager/src/main/java/minerva/android/walletmanager/utils/logger/LoggerImpl.kt
@@ -6,7 +6,12 @@ import minerva.android.walletmanager.manager.wallet.WalletConfigManager
 class LoggerImpl(private val walletConfigManager: WalletConfigManager) : Logger {
 
     override fun logToFirebase(message: String) {
-        FirebaseCrashlytics.getInstance()
-            .recordException(Throwable("PublicKey: ${walletConfigManager.masterSeed.publicKey} $message"))
+        if (::walletConfigManager.masterSeed.isInitialized) {
+            FirebaseCrashlytics.getInstance()
+                .recordException(Throwable("PublicKey: ${walletConfigManager.masterSeed.publicKey} $message"))
+        } else {
+            FirebaseCrashlytics.getInstance()
+                .recordException(Throwable(message))
+        }
     }
 }
