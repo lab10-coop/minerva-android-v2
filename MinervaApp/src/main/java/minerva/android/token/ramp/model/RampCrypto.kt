607296d4f933ccdb58ae86e8c88d9af982bbfe82
PeterTheOne <petertheone@gmail.com>
2022-10-12 19:08:28 +0200

    Add cREAL to buy crypto & fix bugs

--- MinervaApp/src/main/java/minerva/android/token/ramp/model/RampCrypto.kt
+++ MinervaApp/src/main/java/minerva/android/token/ramp/model/RampCrypto.kt
@@ -2,7 +2,8 @@ package minerva.android.token.ramp.model
 
 data class RampCrypto(
     val chainId: Int,
-    val symbol: String,
+    val displaySymbol: String,
+    val apiSymbol: String,
     val iconRes: Int,
     val network: String,
     var isSelected: Boolean = false
