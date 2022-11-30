82f4bb2b2494fab56296a7a4b99079f0a975b6af
vchernov <ftestf32@gmail.com>
2022-11-30 13:58:20 +0200

    GL-156 : set squared and rounded corners for nft icon on "Mannage Assets" page (like on "Values" page)

--- MinervaApp/src/main/java/minerva/android/widget/blockies/BlockiesImageView.kt
+++ MinervaApp/src/main/java/minerva/android/widget/blockies/BlockiesImageView.kt
@@ -5,7 +5,12 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import coil.imageLoader
+import coil.request.ImageRequest
+import coil.transform.RoundedCornersTransformation
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import minerva.android.R
 import minerva.android.kotlinUtils.function.orElse
@@ -50,8 +55,20 @@ open class BlockiesImageView(context: Context, attributeSet: AttributeSet?) : Ap
     }
 
     private fun prepareERCTokenIcon(token: ERCToken) {
-        if (token.type.isNft() && token.logoURI.isNullOrEmpty()) {
+        if (token.type.isNft()) {
             this.setImageResource(R.drawable.ic_collectible_square)//set default logo for nft
+            if (!token.logoURI.isNullOrEmpty()) {//set image from url
+                //create coil image loader request with rounded corners for icon
+                val request = ImageRequest.Builder(this.context)
+                    .transformations(RoundedCornersTransformation(10f))
+                    .placeholder(R.drawable.ic_collectible_square)
+                    .error(R.drawable.ic_collectible_square)
+                    .data(token.logoURI)
+                    .target(this)
+                    .build()
+
+                context.imageLoader.enqueue(request)//set coil image loader request to coil image loader
+            }
         } else {
             token.logoURI?.let { uri ->
                 val icon = when {
