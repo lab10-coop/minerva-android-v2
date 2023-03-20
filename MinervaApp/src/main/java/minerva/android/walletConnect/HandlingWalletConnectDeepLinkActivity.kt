e088a97a11defe04be5e5c94624ace0ada589b95
Peter Grassberger <peter.grassberger@lab10.coop>
2023-03-20 19:33:56 +0100

    WC 2.0: Deeplink with https://minerva.digital/wc url and extract uri param

--- MinervaApp/src/main/java/minerva/android/walletConnect/HandlingWalletConnectDeepLinkActivity.kt
+++ MinervaApp/src/main/java/minerva/android/walletConnect/HandlingWalletConnectDeepLinkActivity.kt
@@ -1,12 +1,15 @@
 package minerva.android.walletConnect
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import minerva.android.R
 import minerva.android.extension.launchActivity
 import minerva.android.main.MainActivity
 import minerva.android.splash.BaseLaunchAppActivity
 import timber.log.Timber
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
 class HandlingWalletConnectDeepLinkActivity : BaseLaunchAppActivity() {
 
@@ -17,11 +20,24 @@ class HandlingWalletConnectDeepLinkActivity : BaseLaunchAppActivity() {
     }
 
     override fun showMainActivity() {
-        Timber.i("Handling deep link with scheme: ${intent?.data?.scheme}")
+        val modifiedData = modifyDeepLinkToWCFormat(intent?.data)
+        Timber.i("Handling deep link with scheme: ${intent?.data} -> $modifiedData")
         launchActivity<MainActivity> {
-            putExtra(BaseWalletConnectInteractionsActivity.MOBILE_WALLET_CONNECT_DATA, intent?.data)
+            putExtra(BaseWalletConnectInteractionsActivity.MOBILE_WALLET_CONNECT_DATA, modifiedData)
             addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
             finish()
+        }
+    }
+
+    private fun modifyDeepLinkToWCFormat(data: Uri?): Uri? {
+        if (data == null) return null
+        val uriString = data.toString()
+        return when (data.scheme) {
+            "https", "minerva" -> {
+                val wcUri = uriString.substringAfter("uri=")
+                Uri.parse(URLDecoder.decode(wcUri, StandardCharsets.UTF_8))
+            }
+            else -> data
         }
     }
 }
