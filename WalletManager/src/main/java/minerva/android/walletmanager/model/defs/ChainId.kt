bbbb608d72b7ae24ea30ac572458db8869ec1036
Peter Grassberger <peter.grassberger@lab10.coop>
2023-04-04 16:37:32 +0200

    Add zkSync Era Mainnet, including ramp.
    rename zkSync testnet

--- WalletManager/src/main/java/minerva/android/walletmanager/model/defs/ChainId.kt
+++ WalletManager/src/main/java/minerva/android/walletmanager/model/defs/ChainId.kt
@@ -28,6 +28,7 @@ import minerva.android.walletmanager.model.defs.ChainId.Companion.OPT_KOV
 import minerva.android.walletmanager.model.defs.ChainId.Companion.OPT_GOR
 import minerva.android.walletmanager.model.defs.ChainId.Companion.OPT_BED
 import minerva.android.walletmanager.model.defs.ChainId.Companion.ZKS_ALPHA
+import minerva.android.walletmanager.model.defs.ChainId.Companion.ZKS_ERA
 import minerva.android.walletmanager.model.defs.ChainId.Companion.POA_CORE
 import minerva.android.walletmanager.model.defs.ChainId.Companion.POA_SKL
 import minerva.android.walletmanager.model.defs.ChainId.Companion.RSK_MAIN
@@ -37,7 +38,7 @@ import minerva.android.walletmanager.model.defs.ChainId.Companion.GNO_CHAI
 
 // why isn't this taken from networks.json?
 @Retention(AnnotationRetention.SOURCE)
-@IntDef(ATS_TAU, ETH_ROP, ETH_RIN, ETH_KOV, ETH_GOR, ETH_SEP, POA_SKL, LUKSO_14, LUKSO_16, ETH_MAIN, ATS_SIGMA, GNO, GNO_CHAI, POA_CORE, MATIC, MUMBAI, BSC, BSC_TESTNET, RSK_MAIN, RSK_TEST, ARB_ONE, ARB_RIN, ARB_GOR, OPT, OPT_KOV, OPT_GOR, OPT_BED, ZKS_ALPHA, CELO, CELO_ALF, CELO_BAK, AVA_C, AVA_FUJ)
+@IntDef(ATS_TAU, ETH_ROP, ETH_RIN, ETH_KOV, ETH_GOR, ETH_SEP, POA_SKL, LUKSO_14, LUKSO_16, ETH_MAIN, ATS_SIGMA, GNO, GNO_CHAI, POA_CORE, MATIC, MUMBAI, BSC, BSC_TESTNET, RSK_MAIN, RSK_TEST, ARB_ONE, ARB_RIN, ARB_GOR, OPT, OPT_KOV, OPT_GOR, OPT_BED, ZKS_ALPHA, ZKS_ERA, CELO, CELO_ALF, CELO_BAK, AVA_C, AVA_FUJ)
 annotation class ChainId {
     companion object {
         const val ATS_TAU = 246785
@@ -68,6 +69,7 @@ annotation class ChainId {
         const val OPT_GOR = 420
         const val OPT_BED = 28528
         const val ZKS_ALPHA = 280
+        const val ZKS_ERA = 324
         const val CELO = 42220
         const val CELO_ALF = 44787
         const val CELO_BAK = 62320
